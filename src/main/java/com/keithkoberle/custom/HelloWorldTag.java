a3d2cfaad13f41a02115ccdcbef7c1e32caac9fa
kdkoberle <kdkoberle@madisoncollege.edu>
2018-09-19 20:12:44 -0500

    first commit

---
+++ src/main/java/com/keithkoberle/custom/HelloWorldTag.java
@@ -0,0 +1,20 @@
+package com.keithkoberle.custom;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+
+public class HelloWorldTag extends SimpleTagSupport {
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        super.doTag();
+
+        JspWriter out = getJspContext().getOut();
+
+        out.println("Hello Custom Tag Section");
+    }
+
+
+}
